e8692d45a4cc8983dc75f29f935449c61e82a666
Yining <yining.wang@utexas.com>
2020-03-19 21:33:15 -0500

    deleted amazon from envs, deleted test env from database.yml

--- config/routes.rb
+++ config/routes.rb
@@ -4,24 +4,24 @@ Rails.application.routes.draw do
   # resources :categories
   # resources :photos
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'api/v1/photos#index'
+  root "api/v1/photos#index"
 
   namespace :api do
     namespace :v1 do
-        get 'photos/animals' => 'photos#animals'
-        get 'photos/civilization' => 'photos#civilization'
-        get 'photos/home' => 'photos#home'
-        get 'photos/homed' => 'photos#homed'
-        get 'photos/lights' => 'photos#lights'
-        get 'photos/man_wild' => 'photos#man_wild'
-        get 'photos/mountains' => 'photos#mountains'
-        get 'photos/plants' => 'photos#plants'
-      # post 'users/login' => 'users#login'
-      # get 'users/login' => 'users#login'
+        get "photos/animals" => "photos#animals"
+        get "photos/civilization" => "photos#civilization"
+        get "photos/home" => "photos#home"
+        get "photos/homed" => "photos#homed"
+        get "photos/lights" => "photos#lights"
+        get "photos/man_wild" => "photos#man_wild"
+        get "photos/mountains" => "photos#mountains"
+        get "photos/plants" => "photos#plants"
+      # post "users/login" => "users#login"
+      # get "users/login" => "users#login"
       # # resources :users, only: [:show]
       # resources :games
       # resources :sessions, only: [:create, :destroy]
-      # root to:'static#home'
+      # root to:"static#home"
     end
   end
 end
